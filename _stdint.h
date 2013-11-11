3c2163397bb7dcef375de62e5b386b283660c1bf
Greg Nietsky <gregory@distrotech.co.za>
2013-11-11 14:04:28 +0200

    CLean

--- _stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GST_PLUGINS_BAD__STDINT_H
-#define _GST_PLUGINS_BAD__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-bad 0.10.23"
-/* generated using gnu compiler gcc (GCC) 4.8.1 20130530 (prerelease) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
